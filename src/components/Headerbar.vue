2c22a60289e7b44377d13187c54d0ed26de8902d
Enkidu <enkidu275@gmail.com>
2022-03-05 18:48:38 -0600

    commit

--- src/components/Headerbar.vue
+++ src/components/Headerbar.vue
@@ -1,24 +1,30 @@
 <template>
     <div>
-        <span class="headerlogo1">
-            <!--The following is the LAR house icon thing-->
-            <svg width="10vw" viewBox="40 20 242 213">
-                <path fill="#CCCCFF" d="M74 116L90 116L90 209L157 209L157 224L74 224L74 116ZM157 29L157 202L141 202L141 60L99 89L141 89L141 105L113 105L113 202L97 202L97 105L48 105L157 29ZM251 224L227 105L274 105L168 31L169 224L185 224L185 62L223 89L185 89L185 105L211 105L236 224L251 224Z"/>
-            </svg>
-        </span>
-        <span class="headerlogo2">
-            aramie Restoration
-        </span>
+        <!--Use "a" to enable tabbing for ADA purposes-->
+        <a href="">
+            <span class="headerlogo1">
+                <!--The following is the LAR house icon thing-->
+                <svg width="10vw" viewBox="40 20 242 213">
+                    <path fill="#CCCCFF" d="M74 116L90 116L90 209L157 209L157 224L74 224L74 116ZM157 29L157 202L141 202L141 60L99 89L141 89L141 105L113 105L113 202L97 202L97 105L48 105L157 29ZM251 224L227 105L274 105L168 31L169 224L185 224L185 62L223 89L185 89L185 105L211 105L236 224L251 224Z"/>
+                </svg>
+            </span>
+            <span class="headerlogo2">
+                aramie<br/>Restoration
+            </span>
+        </a>
+        <a href="" class="headerlogo3">
+            
+        </a>
     </div>
 </template>
 
 <script>
-export default {
-  name: 'Headerbar',
-  props: {
-    username: String
-  }
-}
+    export default {
+        name: 'Headerbar',
+        props: {
+            username: String
+        }
+    }
 </script>
 
 <style scoped>
@@ -33,12 +39,24 @@ export default {
         text-align:left;
     }
     .headerlogo1 {
-        width:10%;
+        float:left;
+        display:inline;
+        width:10vw;
+        height:10vw;
     }
     .headerlogo2 {
-        margin-left:-2vw;
+        float:left;
+        height:10vw;
         color:#CCCCFF;
-        min-width:20%;
-        font:5vw Cambria, sans-seif;
+        font:4vw Cambria, sans-seif;
+    }
+    .headerlogo3 {
+        text-decoration:none;
+        font-style:none;
+        height:10vw;
+        margin-right:2vw;
+        float:right;
+        color:#CCCCFF;
+        font:5vw Cambria, sans-serif;
     }
 </style>
